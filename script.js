575521695c1727d4c5d01f9adcb1b5aa48cc9b3c
Michał Flis <86780656+Michal-Flis@users.noreply.github.com>
2024-06-07 14:17:03 +0200

    Change files and added addEventListener in JS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const btn = document.querySelector('.btn');
+const startBody = document.querySelector('.main-body');
+const thankBody = document.querySelector('.thank-you');
+const ratingBtn = document.querySelectorAll('.rating');
+const span = document.querySelector('span');
+
+btn.addEventListener('click', () => {
+  startBody.style.display = 'none';
+  thankBody.style.display = 'block';
+});
+
+ratingBtn.forEach((rating) => {
+  rating.addEventListener('click', () => {
+    span.innerHTML = rating.innerHTML;
+  });
+});
